219c1fd5496178dd968b0919e49d35671a275bae
hoapha1009 <hoapha1009@gmail.com>
2021-03-25 15:24:01 +0700

    Feat: Build post-item, posts-grid, all-posts and featured posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,46 @@
-import FeaturedComponent from '../components/home-page/featured-component';
 import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-nextjs',
+        title: 'Getting started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, officia porro ut, quae aliquam, reprehenderit perspiciatis commodi suscipit optio accusamus quis. Quasi doloremque adipisci ad nisi, optio voluptas eaque veniam.',
+        date: '2021-03-10',
+    },
+    {
+        slug: 'getting-started-nextjs2',
+        title: 'Getting started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, officia porro ut, quae aliquam, reprehenderit perspiciatis commodi suscipit optio accusamus quis. Quasi doloremque adipisci ad nisi, optio voluptas eaque veniam.',
+        date: '2021-03-10',
+    },
+    {
+        slug: 'getting-started-nextjs3',
+        title: 'Getting started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, officia porro ut, quae aliquam, reprehenderit perspiciatis commodi suscipit optio accusamus quis. Quasi doloremque adipisci ad nisi, optio voluptas eaque veniam.',
+        date: '2021-03-10',
+    },
+    {
+        slug: 'getting-started-nextjs4',
+        title: 'Getting started with NextJS',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, officia porro ut, quae aliquam, reprehenderit perspiciatis commodi suscipit optio accusamus quis. Quasi doloremque adipisci ad nisi, optio voluptas eaque veniam.',
+        date: '2021-03-10',
+    },
+];
 
 export default function Home() {
     return (
         <>
             <Hero />
-            {/* <FeaturedComponent /> */}
+            <FeaturedPosts posts={DUMMY_POSTS} />
         </>
     );
 }
